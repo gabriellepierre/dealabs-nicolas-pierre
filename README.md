d15e14344b0120b5122416cc454fb3e3ba26a0a0
thibault.nicolas <thibault.nicolas@etu.uca.fr>
2023-05-30 09:53:36 +0200

    Création formulaire inscription

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dealabs-nicolas-pierre
 
+Générer les dossiers "vendor" et "var" : composer upgradesymfon
 Utiliser PHP : docker exec -it -u root lpa_sf6_php bash
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route(path: '/security', name: 'security_')]
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -24,7 +28,24 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/registration', name: 'registration')]
+    public function registration(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
